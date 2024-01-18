134234f6cd6478391b282baaa3f28a2a427986c5
JEONGYOUNGGYU <zkdlf90@gmail.com>
2024-01-18 15:03:07 +0900

    controller 수정

--- src/main/kotlin/nbcdocker/learning/cicd_aws/controller/HomeController.kt
+++ src/main/kotlin/nbcdocker/learning/cicd_aws/controller/HomeController.kt
@@ -1,9 +1,11 @@
 package nbcdocker.learning.cicd_aws.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping
 class HomeController {
 
     @GetMapping("/")
